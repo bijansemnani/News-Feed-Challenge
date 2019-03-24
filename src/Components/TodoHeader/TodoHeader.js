6c6b5e4205b21064a4e3f2e0bdcefee31538d397
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2019-03-24 09:50:55 -0700

    restructuring the files

---
+++ src/Components/TodoHeader/TodoHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoHeader extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h1>Todo list</h1>;
+  }
+}
+
+export default TodoHeader;
