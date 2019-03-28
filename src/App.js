a7119fb57e9fc9b3006bcc5544233daeb19841c9
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2019-03-27 18:57:01 -0700

    Pushing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TodoApp from "./Components/TodoApp";
+import NewsFeedApp from "./Components/NewsFeedApp";
 
 import "./App.css";
 
@@ -7,7 +7,7 @@ import "./App.css";
 class App extends React.Component{
   render(){
     return(
-      <TodoApp/>
+      <NewsFeedApp/>
     );
   }
 }
